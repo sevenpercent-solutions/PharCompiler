ce1523fe5e5ea5e4d933f7f7d0a1c51ae2702c42
Steven Hilder <stevehilder@yahoo.co.uk>
2014-08-14 19:07:07 +0000

    Simplify adding of files with relative paths

--- src/Compiler.php
+++ src/Compiler.php
@@ -10,30 +10,26 @@ use SplFileInfo;
 
 class Compiler {
 
-	public function compile($name, array $directories) {
+	public function compile($executable, array $directories) {
 
-		$phar = new Phar("$name.phar", 0, "$name.phar");
+		$basename = basename($executable);
+
+		$phar = new Phar("$basename.phar", 0, "$basename.phar");
 		$phar->setSignatureAlgorithm(Phar::SHA1);
 		$phar->startBuffering();
 
 		foreach ($directories as $directory) {
+			$pathOffset = strlen(dirname(realpath($directory))) + 1;
 			foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory, FilesystemIterator::SKIP_DOTS)) as $file) {
 				if ($file->getExtension() === 'php') {
-					$this->_addFile($phar, $file);
+					$phar->addFile($file->getRealPath(), substr($fileRealPath, $pathOffset));
 				}
 			}
 		}
-		$phar->addFromString("bin/$name", preg_replace('{^#!/usr/bin/env php\s*}', '', file_get_contents(__DIR__ . "/../bin/$name")));
+		$phar->addFromString("bin/$basename", preg_replace('{^#!/usr/bin/env php\s*}', '', file_get_contents($executable)));
 
-		$phar->setStub("#!/usr/bin/env php\n<?php require'phar://$name.phar/bin/$name';__halt_compiler();");
+		$phar->setStub("#!/usr/bin/env php\n<?php require'phar://$basename.phar/bin/$basename';__halt_compiler();");
 
 		$phar->stopBuffering();
-		unset($phar);
-	}
-
-	private function _addFile(Phar $phar, SplFileInfo $file) {
-		$path = strtr(str_replace(dirname(__DIR__) . DIRECTORY_SEPARATOR, '', $file->getRealPath()), '\\', '/');
-		$content = file_get_contents($file);
-		$phar->addFromString($path, $content);
 	}
 }
